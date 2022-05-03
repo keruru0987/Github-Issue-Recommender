30ea05351570c6bbd9574fed876992f34d77c686
1180300708 <61471343+1180300708@users.noreply.github.com>
2022-05-03 17:43:50 +0800

    修改搜索方式，增加tag的影响

--- stackoverflow_search.py
+++ stackoverflow_search.py
@@ -13,23 +13,28 @@ class SOSearcher(object):
     def search(self):
         """
         根据给定的API和查询语句，返回与其相关的so posts，根据相关程度进行降序排序
-        :return: 包含posts信息的列表，信息分别为ID，标题，内容，相关度（用出现次数进行表示）
+        :return: 包含posts信息的列表，信息分别为链接（用ID进行构造），标题，内容，相关度
         """
         fpath = settings.stackoverflow_filepath[self.api]
-        df = pd.read_csv(fpath)
+        sodata_df = pd.read_csv(fpath)
 
         # selected_df = pd.DataFrame(columns=['ID', 'Title', 'Body'])
         result_so = []
 
-        for index, row in df.iterrows():
-            match_num = 0  # 出现次数
+        for index, row in sodata_df.iterrows():
+            match_score = 0
             # 把所有字符转换为小写形式
             for word in self.query.lower().split():
                 if word in row['Title'].lower():
-                    match_num += 1
-            if match_num > 0:
+                    match_score += 1
+                if word in row['Tags'].lower():
+                    match_score += 3
+                # 这里还可以考虑body的影响
+
+            if match_score >= 1:
                 link = 'https://stackoverflow.com/questions/'+str(row['Id'])
-                information = [link, row['Title'], row['Body'], match_num]
+                # 构造其中一条数据
+                information = [link, row['Title'], row['Body'], match_score]
                 result_so.append(information)
 
         return_so = sorted(result_so, reverse=True, key=lambda post: post[3])
@@ -38,7 +43,8 @@ class SOSearcher(object):
 
 
 if __name__ == '__main__':
-    searcher = SOSearcher('TextBlob', 'install error')
+    searcher = SOSearcher('TextBlob', 'install dataframe')
     so = searcher.search()
-    print(so)
+    for ele in so:
+        print(ele)
 
